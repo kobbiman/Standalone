36f585e557dbcced211d6004e6bb8df9db8d9724
Kwabena Owusu <kwabena.owusu@dreambroker.fi>
2016-09-12 13:17:34 +0300

    Add production packaging for webpack

---
+++ webpack.production.config.js
@@ -0,0 +1,58 @@
+'use strict';
+
+require('babel-loader');
+var path = require('path');
+var webpack = require('webpack');
+var del = require('del');
+
+class CleanPlugin {
+    constructor(options) {
+        this.options = options;
+    }
+
+    apply () {
+        del.sync(this.options.files);
+    }
+}
+
+module.exports = {
+    entry: './src/standalone.js',
+    output: {
+        path: __dirname + '/dist',
+        filename: 'standalone.js',
+        library: 'standalone',
+        libraryTarget: 'commonjs2'
+    },
+    plugins: [
+        new webpack.optimize.OccurrenceOrderPlugin(),
+        new CleanPlugin({
+            files: ['dist/*']
+        }),
+        new webpack.optimize.UglifyJsPlugin({
+            compressor: {
+                warnings: false,
+                screw_ie8: true
+            }
+        }),
+        new webpack.DefinePlugin({
+            'process.env':{
+                'NODE_ENV': JSON.stringify('production')
+            }
+        })
+    ],
+    module: {
+        loaders: [
+            {
+                test: /\.js$/,
+                loader: 'babel-loader',
+                include: path.join(__dirname, 'src'),
+                query: {
+                    presets: ['es2015', 'stage-0'],
+                    plugins: [
+                        ['transform-object-assign']
+                    ]
+                }
+            }
+        ]
+    }
+};
